aa4bad7c134dc0c97a0fd055d780b06a244dbac7
Maria Jose Jofre <mariajosejofre97@gmail.com>
2019-04-17 11:12:08 -0300

    otrometodo

--- package pruebagit/src/pkgpackage/pruebagit/PackagePruebagit.java
+++ package pruebagit/src/pkgpackage/pruebagit/PackagePruebagit.java
@@ -18,6 +18,7 @@ public class PackagePruebagit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hola");
     }
 
     private void otrometodo() {
